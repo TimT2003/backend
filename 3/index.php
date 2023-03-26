7fb75da84661222c2a7a09ac6c36619e5cd2bbc4
TimT2003 <112850350+TimT2003@users.noreply.github.com>
2023-03-26 21:10:23 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -3,7 +3,6 @@ header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['save'])) {
-    print('Данные сохранены');
   }
   include('form.php');
 }
