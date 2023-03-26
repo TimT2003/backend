91b8bca4b875e53812cca146b701eeb5694f73f8
TimT2003 <112850350+TimT2003@users.noreply.github.com>
2023-03-26 18:44:18 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -1,6 +1,6 @@
 <style>
     .form1{
-        max-width: 960px;
+        max-width: 700px;
         text-align: center;
         margin: 0 auto;
     }
@@ -16,28 +16,28 @@
     <select name="year">
       <option value="Выбрать">Выбрать</option>
     <?php
-        for($i=1800;$i<=2022;$i++){
+        for($i=2000;$i<=2022;$i++){
           printf("<option value=%d>%d год</option>",$i,$i);
         }
     ?>
     </select> <br>
     <label> Ваш пол </label> <br>
     <div>
-      <input name="sex" type="radio" value="M" /> Мужчина
-      <input name="sex" type="radio" value="W" /> Женщина
+      <input name="gender" type="radio" value="1" /> M
+      <input name="gender" type="radio" value="2" /> Ж
     </div>
     <label> Сколько у вас конечностей </label> <br>
     <div>
       <input name="limb" type="radio" value="1" /> 1 
-      <input name="limb" type="radio" value="2" /> 2 
-      <input name="limb" type="radio" value="3" /> 3 
-      <input name="limb" type="radio" value="4" /> 4 
+      <input name="limb" type="radio" value="2" /> 10
     </div>
     <label> Выберите суперспособности </label> <br>
     <select name="power[]" size="3" multiple>
-      <option value="Телепортация">Телпортация</option>
-      <option value="Бессмертие">Бессмертие</option>
-      <option value="Телепатия">Телепатия</option>
+      <option value="1">Проход сквозь стены</option>
+      <option value="2">Дыхание под водой</option>
+      <option value="3">Ночное зрение</option>
+       <option value="4">Уметь делать сальто назад</option>
+        
     </select> <br>
     <label> Биография </label> <br>
     <textarea name="bio" rows="10" cols="15"></textarea> <br>
