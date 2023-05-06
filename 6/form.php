c54971c16e46eb9bd8a4a5594f47d5318176a4c5
TimT2003 <112850350+TimT2003@users.noreply.github.com>
2023-05-06 21:15:44 +0300

    Update form.php

--- 6/form.php
+++ 6/form.php
@@ -1,11 +1,6 @@
 <head>
   <link rel="stylesheet" href="style.css" type="text/css">
 </head>
-<style>
-  .error {
-    border: 2px solid red;
-  }
-</style>
 <body>
 <?php
 if (!empty($messages)) {
@@ -27,7 +22,7 @@ if (!empty($messages)) {
     <select name="year" <?php if ($errors['year']) {print 'class="error"';} ?>>
       <option value="Выбрать">Выбрать</option>
     <?php
-        for($i=1800;$i<=2022;$i++){
+        for($i=2000;$i<=2022;$i++){
           if($values['year']==$i){
             printf("<option value=%d selected>%d год</option>",$i,$i);
           }
@@ -38,22 +33,20 @@ if (!empty($messages)) {
     ?>
     </select> <br>
     <label> Ваш пол </label> <br>
-    <div <?php if ($errors['sex']) {print 'class="error"';} ?>>
-      <input name="sex" type="radio" value="1" <?php if($values['sex']=="1") {print 'checked';} ?>/> Мужчина
-      <input name="sex" type="radio" value="2" <?php if($values['sex']=="2") {print 'checked';} ?>/> Женщина
+    <div <?php if ($errors['gender']) {print 'class="error"';} ?>>
+      <input name="gender" type="radio" value="1" <?php if($values['gender']=="1") {print 'checked';} ?>/> Мужчина
+      <input name="gender" type="radio" value="2" <?php if($values['gender']=="2") {print 'checked';} ?>/> Женщина
     </div>
     <label> Сколько у вас конечностей </label> <br>
     <div <?php if ($errors['limb']) {print 'class="error"';} ?>>
       <input name="limb" type="radio" value="1" <?php if($values['limb']=="1") {print 'checked';} ?>/> 1 
-      <input name="limb" type="radio" value="2" <?php if($values['limb']=="2") {print 'checked';} ?>/> 2 
-      <input name="limb" type="radio" value="3" <?php if($values['limb']=="3") {print 'checked';} ?>/> 3 
-      <input name="limb" type="radio" value="4" <?php if($values['limb']=="4") {print 'checked';} ?>/> 4 
+      <input name="limb" type="radio" value="2" <?php if($values['limb']=="2") {print 'checked';} ?>/> 10
     </div>
     <label> Выберите суперспособности </label> <br>
-    <select name="form1[]" size="3" multiple <?php if ($errors['form1']) {print 'class="error"';} ?>>
-      <option value="1" <?php if($values['1']==1){print 'selected';} ?>>Бессмертие</option>
-      <option value="2" <?php if($values['2']==1){print 'selected';} ?>>Телепортация</option>
-      <option value="3" <?php if($values['3']==1){print 'selected';} ?>>Телепатия</option>
+    <select name="power[]" size="3" multiple <?php if ($errors['powers']) {print 'class="error"';} ?>>
+      <option value="1" <?php if($values['1']==1){print 'selected';} ?>>Проход сквозь стены</option>
+      <option value="2" <?php if($values['2']==1){print 'selected';} ?>>Дыхание под водой</option>
+      <option value="3" <?php if($values['3']==1){print 'selected';} ?>>Ночное зрение</option>
     </select> <br>
     <label> Краткая биография </label> <br>
     <textarea name="bio" rows="10" cols="15"><?php print $values['bio']; ?></textarea> <br>
